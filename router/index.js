d962bf8091258e1dc2402754a28c231cae5df513
sefati arora <sefatiarora@cqlsys.co.uk>
2025-11-25 15:37:00 +0530

    modified

--- router/index.js
+++ router/index.js
@@ -20,5 +20,14 @@ router.get("/api", async (req, res) => {
   return res.status(200).send(jsonData);
 });
 
-
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ1ZDhjNmJkLWFmMDgtNDlkMi1hZDllLWJmMDI2ZmU5MGVlYSIsImlhdCI6MTc2NDA2NDEyNH0.nJblyViyOALisJMYNgETMPu69LfFWhvI9qKHdK-91q0
 module.exports = router;
+//ccfadb75-ad3d-4a43-a238-7d71a81be532 //addressId
+
+//b3351610-9a46-4995-b6cd-0dde590601d3  //productId
+
+//461535eb-51e9-4d82-9262-ee235e5b4ede //cartId
+
+//70975207-1c19-420c-bd3c-3a5e7473888b //storeId
+
+//45d8c6bd-af08-49d2-ad9e-bf026fe90eea  //assignDriver
